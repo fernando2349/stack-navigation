5fbb807dbf173cf678927cff6558945d76660a7b
fernando2349 <146887271+fernando2349@users.noreply.github.com>
2024-04-07 03:44:24 -0500

    main
    Vamos a empezar con el Stack navegator

---
+++ lab-react-native-with-cli-navigation/src/presentation/App.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+const App = () => {
+  return(
+  <NavigationContainer>
+    <Text>App</Text>
+  </NavigationContainer>
+  )
+  
+}
+export default App;
