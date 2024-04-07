b61308528c73832ad6340427d22614c2186be572
fernando2349 <146887271+fernando2349@users.noreply.github.com>
2024-04-07 03:05:44 -0500

    main
    esquema de pantallas por ahora

---
+++ lab-react-native-with-cli-navigation/src/presentation/screens/Products/ProductsScreen.tsx
@@ -0,0 +1,12 @@
+import {Text, View} from "react-native";
+
+const ProductsScreen = () => {
+    return(
+    <View>
+         <Text>Product-Screen</Text>
+         </View>
+         )
+
+}
+
+export default ProductsScreen;
---
+++ lab-react-native-with-cli-navigation/src/presentation/screens/tabs/Tab01Screen.tsx
@@ -0,0 +1,12 @@
+import {Text, View} from "react-native";
+
+const Tab01Screen = () => {
+    return(
+    <View>
+         <Text>Tab01-Screen</Text>
+         </View>
+         )
+
+}
+
+export default Tab01Screen;
