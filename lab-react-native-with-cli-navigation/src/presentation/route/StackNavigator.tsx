5a3e1fe0a39b9a15372f8a70ab39d2c7bc8a148e
fernando2349 <146887271+fernando2349@users.noreply.github.com>
2024-04-07 15:21:29 -0500

    main
    tabla productos

--- lab-react-native-with-cli-navigation/src/presentation/route/StackNavigator.tsx
+++ lab-react-native-with-cli-navigation/src/presentation/route/StackNavigator.tsx
@@ -1,4 +1,4 @@
-import { createStackNavigator } from '@react-navigation/stack';
+import { HeaderStyleInterpolators, createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from '../screens/home/HomeScreen';
 import ProductScreen from '../screens/Products/ProductScreen';
 import SettingsScreen from '../screens/Settings/SettingsScreen';
@@ -8,7 +8,15 @@ const Stack = createStackNavigator();
 
 function StackNavigator() {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator screenOptions={
+    {
+      headerShown : true,
+      headerStyle: {
+        elevation : 0,
+        shadowColor : 'transparent',
+      }
+    }
+    }>
       <Stack.Screen name="Home" component={HomeScreen} />
       <Stack.Screen name="Products" component={ProductScreen} />
       <Stack.Screen name="Settings" component={SettingsScreen} />
--- lab-react-native-with-cli-navigation/src/presentation/screens/Products/ProductsScreen.tsx
+++ lab-react-native-with-cli-navigation/src/presentation/screens/Products/ProductsScreen.tsx
@@ -1,12 +1,44 @@
 import {Text, View} from "react-native";
+import HomeScreen from "../home/HomeScreen";
+import ProductScreen from './ProductScreen';
+import { globalStyles } from "../../theme/theme";
+import { FlatList } from "react-native-gesture-handler";
+import PrimaryButton from "../../components/PrimaryButton";
 
-const ProductsScreen = () => {
-    return(
-    <View>
-         <Text>Product-Screen</Text>
-         </View>
-         )
+const productos = [
+    { id: 1, name: 'Camiseta de algodón', price: 20.99, category: 'Ropa' },
+    { id: 2, name: 'Zapatos deportivos', price: 49.99, category: 'Calzado' },
+    { id: 3, name: 'Portátil HP', price: 799.99, category: 'Tecnología' },
+    { id: 4, name: 'Libro de cocina', price: 14.50, category: 'Libros' },
+    { id: 5, name: 'Gafas de sol polarizadas', price: 35.75, category: 'Accesorios' },
+    { id: 6, name: 'Reloj inteligente', price: 129.99, category: 'Tecnología' }
+  ];
+  const ProductsScreen = () => {
+    return (
+        <View style={globalStyles.container}>
+            <Text style={{marginBottom:10, fontSize:20}}>Productos</Text>
 
+
+                {/* FlatList para renderizar productos */}
+                <FlatList
+                    data={productos}
+                    renderItem={
+                        ({item }) => (
+                        // Componente PrimaryButton para cada producto
+                        <PrimaryButton
+                            onPress={() : void => {}}
+                                // Lógica para manejar el botón presionado
+                            label={item.name}
+                        />
+                    )
+                }
+                />
+
+                {/* Título para la sección de ajustes */}
+                <Text style={{ marginBottom: 10, fontSize: 20 }}>Ajustes</Text>
+            </View>
+    )
 }
 
 export default ProductsScreen;
+
