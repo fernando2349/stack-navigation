ae3bea75b8e506f68623511ed87a0208340390df
fernando2349 <146887271+fernando2349@users.noreply.github.com>
2024-04-11 01:18:48 -0500

    main
    Laboratorio terminado

--- lab-react-native-with-cli-navigation/src/presentation/screens/Settings/SettingsScreen.tsx
+++ lab-react-native-with-cli-navigation/src/presentation/screens/Settings/SettingsScreen.tsx
@@ -1,9 +1,16 @@
+import { StackActions, useNavigation } from "@react-navigation/native";
 import {Text, View} from "react-native";
+import { globalStyles } from '../../theme/theme';
+import PrimaryButton from "../../components/PrimaryButton";
 
 const SettingsScreen = () => {
+    const navigator = useNavigation();
     return(
-    <View>
-         <Text>Settings-Screen</Text>
+    <View style = {globalStyles.container}>
+         <Text style = {{marginBottom: 10}}>Settings-Screen</Text>
+
+         <PrimaryButton label="regresar" onPress={()=>navigator.goBack}/>
+         <PrimaryButton label="Home" onPress={()=>navigator.dispatch(StackActions.popToTop())}/>
          </View>
          )
 
